492a4942fa76e3c552cd8814789aae58aab720b2
yuraMovsesyan <movsesyan-03@inbox.ru>
2022-07-28 18:22:55 +0300

    refactoring

--- Sources/TerminalCalculator/main.swift
+++ Sources/TerminalCalculator/main.swift
@@ -1,9 +1,8 @@
-print(CommandLine.arguments)
-
 var arguments: String
 
 if CommandLine.arguments.count == 1{
-    arguments = "2 + (2 - 3)"
+    print("> ", terminator: "")
+    arguments = readLine()!
 }else{
     arguments = ""
     for arg in CommandLine.arguments[1...]{
@@ -11,68 +10,4 @@ if CommandLine.arguments.count == 1{
     }
 }
 
-let tokens = Lexer(line: arguments)
-tokens.printTokens()
-
-let numberStack = Stack<Double>()
-let operationStack = Stack<Token>()
-
-for token in tokens.getTokens(){
-    print(token.value, terminator: "")
-
-    if token.typeToken == Token.type.number{
-        numberStack.push(Double(token.value)!)
-    }else if (token.typeToken == Token.type.operation){
-        while let opr = operationStack.pop(){
-            if opr.value != "("{
-                if Operation.getPriority(Character(opr.value)) >= Operation.getPriority(Character(token.value)){
-                    let b = numberStack.pop()!
-                    let a = numberStack.pop()!
-
-                    numberStack.push(Operation.calculate(opr: Character(opr.value), a: a, b: b)!)
-                }
-                else{
-                    operationStack.push(opr)
-                    break 
-                }
-            }else{
-                operationStack.push(opr)
-                break
-            }
-        }
-
-        operationStack.push(token)
-    }else{
-        if token.value == "("{
-            operationStack.push(token)
-        }else{
-            while let opr = operationStack.pop(){
-                if opr.value != "("{
-                    let b = numberStack.pop()!
-                    let a = numberStack.pop()!
-
-                    numberStack.push(Operation.calculate(opr: Character(opr.value), a: a, b: b)!)
-                }else{
-                    operationStack.push(opr)
-                    break
-                }
-            }
-
-            if let opr = operationStack.pop(){
-                if opr.value != "("{
-                    operationStack.push(opr)
-                    operationStack.push(token)
-                }
-            }
-        }
-    }
-}
-
-while let opr = operationStack.pop(){
-    let b = numberStack.pop()!
-    let a = numberStack.pop()!
-
-    numberStack.push(Operation.calculate(opr: Character(opr.value), a: a, b: b)!)
-}
-print()
-print(numberStack.pop()!)
+print (Calculator.calculate(tokens: (Lexer(line: arguments)).getTokens()))
